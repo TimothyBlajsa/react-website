2a5a97f280b80901eb12670e7c67419a721bdae5
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-14 11:32:01 -0400

    Link home

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path='/' exact element={<Home />} />
+        <Route exact path='/home' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/blogs' element={<Blogs />} />
